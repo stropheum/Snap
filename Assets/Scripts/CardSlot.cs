a3e6c90dd8a8439997932434831d49f8a5b3741b
Stropheum <stropheum060@gmail.com>
2024-12-30 17:18:03 -0500

    changed gizmo color

--- Assets/Scripts/CardSlot.cs
+++ Assets/Scripts/CardSlot.cs
@@ -8,7 +8,7 @@ namespace Snap
         private void OnDrawGizmos()
         {
             Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
-            Gizmos.color = IsOccupied ? Color.green : Color.cyan;
+            Gizmos.color = IsOccupied ? Color.red : Color.cyan;
             Gizmos.DrawSphere(Vector3.zero, 0.25f);
             Gizmos.DrawWireCube(Vector3.zero, new Vector3(5.25f, 7.25f, 0.1f));
         }
